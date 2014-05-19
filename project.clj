7df248e417e50368a9e59d5f74f2a435d6304964
Darrell Hamilton <darrell.noice@gmail.com>
2014-05-18 20:52:30 -0600

    pom additions

--- project.clj
+++ project.clj
@@ -1,4 +1,12 @@
 (defproject matross/crosshair "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "https://github.com/matross/crosshair"
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :pom-addition [:developers [:developer
+                              [:id "zeroem"]
+                              [:name "Darrell Hamilton"]
+                              [:url "https://github.com/zeroem"]]
+                 [:developer
+                  [:id "eggsby"]
+                  [:name "Thomas Omans"]
+                  [:url "https://github.com/eggsby"]]])
