ec4fc874c34cc7731fdb31924c94be7bd8d2339c
Darrell Hamilton <darrell.noice@gmail.com>
2014-05-26 00:45:40 -0700

    License elements

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject matross/crosshair "0.2.0-SNAPSHOT"
   :description "Namespaced map keys with a default namespace"
   :url "https://github.com/matross/crosshair"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :pom-addition [:developers [:developer
                               [:id "zeroem"]
